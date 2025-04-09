ce77848eb4dbf1719bc5e06170bb96d13bd95242
Tucker <tucker@fakemail.com>
2025-04-09 14:11:53 -0600

    Configure GitHub Pages deployment for app and Storybook

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tkr-portfolio/', // Add base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
